114ba1a5532096adb23244d65215d28fa21893cc
Vishal <vishaldas6566@gmail.com>
2023-09-14 22:52:02 +0530

    added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "https://trupti-dashboardcumproduct.netlify.app", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 //Routes imports
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
